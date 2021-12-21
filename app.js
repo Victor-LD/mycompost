3cd8c268e873e14490ce36641b54b1701abef245
Victor He <victor.he@lexicondigital.com.au>
2021-12-21 14:41:47 +1100

    adds get endpoint for single listing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
 const dbo = require("./conn.js")
 const app = express()
 const port = process.env.PORT || 3000
@@ -26,7 +27,24 @@ app.get('/listings', (req, res) => {
     });
 })
 
-// C
+// View a listing
+app.get('/listings/:id', (req, res) => {
+
+  const dbConnect = dbo.getDb();
+
+  dbConnect
+    .collection("listings")
+    .findOne({"_id": new ObjectId(req.params.id)}, function (err, result) {
+      if (err) {
+        res.status(400).send(`Error fetching listing with id: ${req.params.id}!`);
+     } else {
+       console.log("Result is " + result)
+        res.json(result);
+      }
+    });
+})
+
+// Creates a listing
 app.post('/listings', (req, res) => {
 
   const dbConnect = dbo.getDb();
